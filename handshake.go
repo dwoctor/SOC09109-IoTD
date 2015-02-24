9502771e8bf450970d22956be499243ece8d0664
dwoctor <dwoctor@users.noreply.github.com>
2015-02-24 22:23:49 +0000

    Updated handshake.go
    Fixed reqLen declared and not used error.

--- handshake.go
+++ handshake.go
@@ -43,11 +43,10 @@ func handleRequest(conn net.Conn) {
 	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
-	reqLen, err := conn.Read(buf)
+	_, err := conn.Read(buf)
 	if err != nil {
 		panic(err)
 	}
-	
 	ip, err := externalIP()
 	if err != nil {
 		panic(err)

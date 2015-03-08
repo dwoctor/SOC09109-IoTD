26d862b56742e8b6bcebcd07395201c026444aa3
David Cochrane <dwoctor@outlook.com>
2015-03-08 12:45:38 +0000

    Added GPIO command handling.

---
+++ GpioCommand.py
@@ -0,0 +1,22 @@
+__author__ = 'David'
+
+import json
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO! You need root privileges.")
+
+
+class GpioCommand(object):
+    def __init__(self, j):
+        self.__dict__ = json.loads(j)
+
+    def execute(self):
+        # disable warning when pin is used in another script
+        GPIO.setwarning(False)
+        # Using channel numbers of the Broadcom SOC
+        GPIO.setmode(GPIO.BCM)
+        # pin 4 is an output
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, self.state)
+        print('PIN %s is %s' % self.pin, 'ON' if self.state else 'OFF')
---
+++ ThreadedController.py
@@ -0,0 +1,37 @@
+__author__ = 'David'
+
+import socketserver
+import threading
+import ThreadedTCPServer
+import GpioCommand
+
+
+class ThreadedControllerTCPRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        GpioCommand(self.request.recv(1024)).execute()
+
+
+class ThreadedController():
+    @staticmethod
+    def __start(daemon):
+        host, port = "localhost", 9999
+        server = ThreadedTCPServer((host, port), ThreadedControllerTCPRequestHandler)
+        ip, port = server.server_address
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = daemon
+        server_thread.start()
+        print('Controller running.')
+
+    @staticmethod
+    def start():
+        ThreadedController.__start(False)
+
+    @staticmethod
+    def start_in_daemon_mode():
+        ThreadedController.__start(True)
+
+if __name__ == "__main__":
+    ThreadedController.start()

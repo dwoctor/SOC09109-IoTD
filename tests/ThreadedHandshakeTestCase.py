ae8641c36a53f3cb7430d5296276fbd96d8c3433
David Cochrane <dwoctor@outlook.com>
2015-03-15 15:21:42 +0000

    Updated tests.

--- tests/ThreadedHandshakeTestCase.py
+++ tests/ThreadedHandshakeTestCase.py
@@ -9,24 +9,21 @@ class ThreadedHandshakeTestCase(unittest.TestCase):
 
     def testPiHandshake(self):
         ThreadedHandshake.start_in_daemon_mode()
-        host, port = 'localhost', 9999
-        data = 'hello'
+        host, port = 'localhost', 1111
 
         # Create a socket (SOCK_STREAM means a TCP socket)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            # Connect to server and send data
+            # Connect to server
             sock.connect((host, port))
-            sock.sendall(bytes('{0}\n'.format(data), 'utf-8'))
 
             # Receive data from the server and shut down
             received = str(sock.recv(1024), 'utf-8')
         finally:
             sock.close()
-        print('Sent:     {}'.format(data))
         print('Received: {}'.format(received))
-        self.assertEqual({'name': 'pi', 'type': 'WiFi'}, received)
+        self.assertEqual({'name': 'pi', 'type': 'wifi', 'capability': 'gpio'}, received)
 
 if __name__ == '__main__':
     unittest.main()

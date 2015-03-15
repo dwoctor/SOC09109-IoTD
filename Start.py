57e4fd7579b79cb91e3ddaaa3f82ff7b6b32d15a
David Cochrane <dwoctor@outlook.com>
2015-03-15 16:13:40 +0000

    Updated startup script to include state service.

--- Start.py
+++ Start.py
@@ -2,6 +2,8 @@ __author__ = 'David'
 
 from ThreadedController import ThreadedController
 from ThreadedHandshake import ThreadedHandshake
+from ThreadedState import ThreadedState
 
 ThreadedController.start()
 ThreadedHandshake.start()
+ThreadedState.start()

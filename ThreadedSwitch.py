736c3cf0a01525bd6c32404399f1c64f37b979a2
David Cochrane <dwoctor@outlook.com>
2015-03-30 17:14:19 +0100

    Updated ThreadedSwitch to use GpioInput for the switch.

--- ThreadedSwitch.py
+++ ThreadedSwitch.py
@@ -5,6 +5,7 @@ import threading
 
 from GpioCommand import GpioCommand
 from GpioState import GpioState
+from GpioInput import GpioInput
 
 
 class ThreadedSwitch():
@@ -12,10 +13,10 @@ class ThreadedSwitch():
     def __start():
         print('SwitchState running.')
         while True:
-            switch = GpioState(json.dumps({'pin': 22}))
+            switch = GpioInput(json.dumps({'pin': 22}))
             if switch.state == 0:
                 while switch.state == 0:
-                    switch = GpioState(json.dumps({'pin': 22}))
+                    switch = GpioInput(json.dumps({'pin': 22}))
                 led = GpioState(json.dumps({'pin': 17}))
                 GpioCommand(json.dumps({'pin': led.pin, 'state': not led.state})).execute()
 
